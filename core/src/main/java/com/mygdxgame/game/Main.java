8a6bf01c810ea25d198df8e873f164c82543b1ec
LAU PENG JIE <league2333@gmail.com>
2025-01-17 15:46:52 +0800

    Update Main.java

--- core/src/main/java/com/mygdxgame/game/Main.java
+++ core/src/main/java/com/mygdxgame/game/Main.java
@@ -8,6 +8,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+//test
+
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
